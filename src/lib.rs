5ee44cb8fb77b5214bc739069dacbf0918c86d26
ben <ben@vivotek.com>
2024-11-04 08:53:04 +0000

    Implement addition function with tests and update main to use it

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(5, 6), 11);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,4 @@
 fn main() {
     println!("Hello, world!");
-    println!("The sum of 5 and 6 is {}", add(5, 6));
-}
-
-fn add(a: i32, b: i32) -> i32 {
-    a + b
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_add() {
-        assert_eq!(add(5, 6), 11);
-    }
+    println!("The sum of 5 and 6 is {}", rust::add(5, 6));
 }
